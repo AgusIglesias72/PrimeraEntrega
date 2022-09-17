2be6f6ae553f77849bb399ed7b04efc2748482b5
AgusIglesias72 <enpalabrass@gmail.com>
2022-09-17 17:00:58 -0300

    change in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,47 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+class AccessDependenciesPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap(
+      'AccessDependenciesPlugin',
+      (compilation) => {
+        compilation.hooks.finishModules.tap(
+          'AccessDependenciesPlugin',
+          (modules) => {
+            /*
+        |---------------------------------------------------
+        | Here we go, `modules` is what we're looking for!
+        |---------------------------------------------------
+        */
+          }
+        )
+      }
+    )
+  }
+}
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
-  target: "node",
+  mode: 'production',
+  entry: './src/index.ts',
+  target: 'node',
   externals: [nodeExternals()],
 
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
+  plugins: [new AccessDependenciesPlugin()],
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
-};
+}
