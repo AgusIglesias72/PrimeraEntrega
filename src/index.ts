6564bacec350c488e3e6933a775b58b729e072a9
AgusIglesias72 <enpalabrass@gmail.com>
2022-09-20 16:56:45 -0300

    Agrego static files

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/build')))
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/build', 'index.html'))
+})
 
 app.use('/api/products/', productsRouter)
 app.use('/api/shipcart/', shipCartRouter)
@@ -23,4 +27,5 @@ app.use('/api/store/', storeRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running port ${PORT}`)
+  console.log(path.join(__dirname, '/build'))
 })
