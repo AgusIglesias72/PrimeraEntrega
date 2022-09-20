a16c3e2f1b275766d6699e8abac08bfc7ce4a06c
AgusIglesias72 <enpalabrass@gmail.com>
2022-09-20 20:43:30 -0300

    Adding static

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,10 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, '../src/frontend/build')))
+app.use(express.static(path.join(__dirname, '/build')))
 
 app.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../src/frontend/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '/build', 'index.html'))
 })
 
 app.use('/api/products/', productsRouter)
@@ -27,5 +27,5 @@ app.use('/api/store/', storeRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running port ${PORT}`)
-  console.log(path.join(__dirname, '../src/frontend/build'))
+  console.log(path.join(__dirname, '../build'))
 })
